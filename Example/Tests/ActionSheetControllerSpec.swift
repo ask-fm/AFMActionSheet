10a559c4bf600e525052f6f8ecc13bbfa4217801
Ilya Alesker <ilya.alesker@ask.fm>
2016-10-11 11:44:50 +0300

    Updated tests to Swift 3

--- Example/Tests/ActionSheetControllerSpec.swift
+++ Example/Tests/ActionSheetControllerSpec.swift
@@ -11,8 +11,8 @@ class ActionSheetControllerSpec: QuickSpec {
             it("should have correct transition delegate, correct presentation style and controller style") {
                 let controller = AFMActionSheetController(transitioningDelegate: AFMActionSheetTransitioningDelegate())
                 expect(controller.transitioningDelegate).toNot(beNil())
-                expect(controller.modalPresentationStyle).to(equal(UIModalPresentationStyle.Custom))
-                expect(controller.controllerStyle).to(equal(AFMActionSheetController.ControllerStyle.ActionSheet))
+                expect(controller.modalPresentationStyle).to(equal(UIModalPresentationStyle.custom))
+                expect(controller.controllerStyle).to(equal(AFMActionSheetController.ControllerStyle.actionSheet))
             }
 
             it("should have gesture recognizers set up") {
@@ -33,25 +33,25 @@ class ActionSheetControllerSpec: QuickSpec {
             }
 
             it("should have correct number of actions and subviews when adding 1 action") {
-                controller.addAction(action)
+                controller.add(action)
                 expect(controller.actions.count).to(equal(1))
                 expect(controller.actionControls.count).to(equal(1))
                 expect(controller.actionGroupView.subviews.count).to(equal(1))
             }
 
             it("should have correct number of actions and subviews when adding 2 actions") {
-                controller.addAction(action)
-                controller.addAction(action)
+                controller.add(action)
+                controller.add(action)
                 expect(controller.actions.count).to(equal(2))
                 expect(controller.actionControls.count).to(equal(2))
                 expect(controller.actionGroupView.subviews.count).to(equal(2))
             }
 
             it("should have correct number of actions and subviews when inserting an action") {
-                controller.addAction(action)
-                controller.addAction(action)
-                let insertedControl = UIButton.controlWithAction(action)
-                controller.insertAction(action, control: insertedControl, position: 0)
+                controller.add(action)
+                controller.add(action)
+                let insertedControl = UIButton.control(with: action)
+                controller.insert(action, with: insertedControl, at: 0)
                 expect(controller.actions.count).to(equal(3))
                 expect(controller.actionControls.count).to(equal(3))
                 expect(controller.actionControls.first).to(equal(insertedControl))
@@ -59,10 +59,10 @@ class ActionSheetControllerSpec: QuickSpec {
             }
 
             it("should have correct number of actions and subviews when inserting an action at wrong position") {
-                controller.addAction(action)
-                controller.addAction(action)
-                let insertedControl = UIButton.controlWithAction(action)
-                controller.insertAction(action, control: insertedControl, position: 42)
+                controller.add(action)
+                controller.add(action)
+                let insertedControl = UIButton.control(with: action)
+                controller.insert(action, with: insertedControl, at: 42)
                 expect(controller.actions.count).to(equal(2))
                 expect(controller.actionControls.count).to(equal(2))
                 expect(controller.actionControls.first).toNot(equal(insertedControl))
@@ -71,12 +71,12 @@ class ActionSheetControllerSpec: QuickSpec {
 
             it("should have correct number of subviews when adding title view") {
                 let titleView = UIView()
-                controller.addTitleView(titleView)
+                controller.add(title: titleView)
                 expect(controller.actionGroupView.subviews.count).to(equal(1))
             }
 
             it("should have label as title view view when adding title") {
-                controller.addTitle("title")
+                controller.add(titleLabelWith: "title")
                 expect(controller.titleView as? UILabel).toNot(beNil())
                 expect((controller.titleView as! UILabel).text).to(equal("title"))
                 expect(controller.actionGroupView.subviews.count).to(equal(1))
@@ -84,14 +84,14 @@ class ActionSheetControllerSpec: QuickSpec {
 
             it("should have control enabled if added action was enabled") {
                 action.enabled = true
-                controller.addAction(action)
-                expect(controller.actionControls.first?.enabled).to(beTrue())
+                controller.add(action)
+                expect(controller.actionControls.first?.isEnabled).to(beTrue())
             }
 
             it("should have control disabled if added action was disabled") {
                 action.enabled = false
-                controller.addAction(action)
-                expect(controller.actionControls.first?.enabled).to(beFalse())
+                controller.add(action)
+                expect(controller.actionControls.first?.isEnabled).to(beFalse())
             }
 
             it("should call correct action's handler when tapping on correspoding control") {
@@ -103,8 +103,8 @@ class ActionSheetControllerSpec: QuickSpec {
                 let action2 = AFMAction(title: "action2", enabled: true) { _ in
                     calledAction2 = true
                 }
-                controller.addAction(action1)
-                controller.addAction(action2)
+                controller.add(action1)
+                controller.add(action2)
                 controller.handleTaps(controller.actionControls.first!)
                 expect(calledAction1).to(beTrue())
                 expect(calledAction2).to(beFalse())
@@ -115,7 +115,7 @@ class ActionSheetControllerSpec: QuickSpec {
                 let action1 = AFMAction(title: "action1", enabled: false) { _ in
                     calledAction1 = true
                 }
-                controller.addAction(action1)
+                controller.add(action1)
                 controller.handleTaps(controller.actionControls.last!)
                 expect(calledAction1).to(beFalse())
             }
@@ -136,17 +136,17 @@ class ActionSheetControllerSpec: QuickSpec {
             }
 
             it("should have correct number of actions and subviews when adding 1 cancel action") {
-                controller.addCancelAction(action)
+                controller.add(cancelling: action)
                 expect(controller.actions.count).to(equal(1))
                 expect(controller.cancelControls.count).to(equal(1))
                 expect(controller.cancelGroupView.subviews.count).to(equal(1))
             }
 
             it("should have correct number of actions and subviews when adding different actions") {
-                controller.addAction(action)
-                controller.addCancelAction(action)
-                controller.addCancelAction(action)
-                controller.addAction(action)
+                controller.add(action)
+                controller.add(cancelling: action)
+                controller.add(cancelling: action)
+                controller.add(action)
                 expect(controller.actions.count).to(equal(4))
                 expect(controller.actionControls.count).to(equal(2))
                 expect(controller.cancelControls.count).to(equal(2))
@@ -160,7 +160,7 @@ class ActionSheetControllerSpec: QuickSpec {
             var action : AFMAction!
 
             beforeEach {
-                controller = MockActionSheetController(style: .Alert)
+                controller = MockActionSheetController(style: .alert)
                 action = AFMAction(title: "action", enabled: true, handler: nil)
             }
 
@@ -170,17 +170,17 @@ class ActionSheetControllerSpec: QuickSpec {
             }
 
             it("should have correct number of actions and subviews when adding 1 cancel action") {
-                controller.addCancelAction(action)
+                controller.add(cancelling: action)
                 expect(controller.actions.count).to(equal(1))
                 expect(controller.cancelControls.count).to(equal(1))
                 expect(controller.cancelGroupView.subviews.count).to(equal(0))
             }
 
             it("should have correct number of actions and subviews when adding different actions") {
-                controller.addAction(action)
-                controller.addCancelAction(action)
-                controller.addCancelAction(action)
-                controller.addAction(action)
+                controller.add(action)
+                controller.add(cancelling: action)
+                controller.add(cancelling: action)
+                controller.add(action)
                 expect(controller.actions.count).to(equal(4))
                 expect(controller.actionControls.count).to(equal(2))
                 expect(controller.cancelControls.count).to(equal(2))
@@ -191,32 +191,32 @@ class ActionSheetControllerSpec: QuickSpec {
 
         describe("AFMActionSheetController", {
             it("has valid snapshot for action sheet setup") {
-                let controller = AFMActionSheetController(style: .ActionSheet, transitioningDelegate: AFMActionSheetTransitioningDelegate())
-                self.setupController(controller)
+                let controller = AFMActionSheetController(style: .actionSheet, transitioningDelegate: AFMActionSheetTransitioningDelegate())
+                self.setup(controller: controller)
                 expect(controller.view).to(haveValidSnapshot(named: "AFMActionSheetController-ActionSheet-Snapshot"))
             }
 
             it("has valid snapshot for alert setup") {
-                let controller = AFMActionSheetController(style: .Alert, transitioningDelegate: AFMActionSheetTransitioningDelegate())
-                self.setupController(controller)
+                let controller = AFMActionSheetController(style: .alert, transitioningDelegate: AFMActionSheetTransitioningDelegate())
+                self.setup(controller: controller)
                 expect(controller.view).to(haveValidSnapshot(named: "AFMActionSheetController-Alert-Snapshot"))
             }
         });
     }
 
-    func setupController(controller: AFMActionSheetController) {
+    func setup(controller: AFMActionSheetController) {
         let action1 = AFMAction(title: "Action 1", enabled: true, handler: nil)
         let action2 = AFMAction(title: "Action 2", enabled: false, handler: nil)
         let action3 = AFMAction(title: "Action 3", handler: nil)
 
-        controller.addAction(action1)
-        controller.addAction(action2)
-        controller.addCancelAction(action3)
-        controller.addTitle("Title")
+        controller.add(action1)
+        controller.add(action2)
+        controller.add(cancelling: action3)
+        controller.add(titleLabelWith: "Title")
     }
 
     class MockActionSheetController : AFMActionSheetController {
-        override func dismissViewControllerAnimated(flag: Bool, completion: (() -> Void)?) {
+        override func dismiss(animated flag: Bool, completion: (() -> Void)?) {
             completion?()
         }
     }
